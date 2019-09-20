a1da4274a3c0ddedba53eb8d5b8e922eb37f2cb4
sandtr0ut <crosbyverse@gmail.com>
2019-09-20 14:07:17 -0700

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # bamazon
+
+<p>
+    <img alt="Stack: Back-End" src="https://img.shields.io/static/v1?label=stack&message=back-end&color=important&style=flat-square" />
+    <img alt="Version" src="https://img.shields.io/badge/version-1.2.7-blue.svg?cacheSeconds=2592000&style=flat-square" />
+    <a href="https://github.com/sandtr0ut/bamazon#readme">
+    <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen?style=flat-square" target="_blank" />
+    </a>
+    <a href="https://github.com/sandtr0ut/bamazon/graphs/commit-activity">
+    <img alt="Maintenance" src="https://img.shields.io/badge/Maintained%3F-yes-blueviolet?style=flat-square" target="_blank" />
+    </a>
+    <a href="https://github.com/sandtr0ut/bamazon/issues">
+    <img alt="Issues" src="https://img.shields.io/github/issues/sandtr0ut/crystals-collector?style=flat-square" target="_blank" />
+    </a>
+    <a href="https://github.com/sandtr0ut/bamazon/blob/master/README.md">
+    <img alt="Website" src="https://img.shields.io/badge/Website%3F-n/a-inactive?style=flat-square" target="_blank">
+    </a>
+    <a href="https://github.com/sandtr0ut/bamazon/blob/master/LICENSE">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow?style=flat-square" target="_blank" />
+  </a>
+</p>
+
+Bamazon is an ultra-simple, 100% back-end, storefront application. It is written in Node.js with the Inquirer npm package and accessed by command line interface. All product information is stored in a MySQL database.
+
+### 🏠 [Homepage](https://github.com/sandtr0ut/burger#readme)
+
+## Install
+
+```
+npm install
+```
+
+## Usage
+
+```
+1. node bamazonCustomer.js
+
+2. Review the displayed inventory
+
+3. Follow prompts to place an order
+```
+
+> Bamazon will alert you if your order quantity exceeds available inventory, then will prompt you for another request.
+
+> Once you place an order, the items you selected will be removed from inventory and Bamazon will display the updated available quantity.
+
+## Demo Images
+
+![initialDisplay](assets/images/initialDisplay.jpg)
+
+#### Prompts
+
+> `? What would you would like to purchase? Please enter item id or Q to quit:`
+
+> `? How many would you like? Please enter quantity:`
+
+#### Order Confirmation - Success
+
+> `Confirmation: You purchased 4 G.Skill Ripjaws V Series 64GB (4x16GB) DDR$ SDRAM`
+
+![display2](assets/images/display2.jpg)
+
+#### Order Confirmation - Insufficient Stock
+
+> `Not enough in stock. Please try a smaller quantity.`
+
+![display3](assets/images/display3.jpg)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,112 +1,121 @@
-var inquirer = require("inquirer");
-var mysql = require("mysql");
+var inquirer = require('inquirer');
+var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: 'localhost',
   port: 3306,
-  user: "root",
-  password: "B8master!",
-  database: "bamazonDB"
+  user: 'root',
+  password: 'root',
+  database: 'bamazonDB'
 });
 
 connection.connect(function(err) {
   if (err) {
-      console.error("connection error: " + err.stack);
+    console.error('connection error: ' + err.stack);
   }
   showTable();
 });
 
 // Display table listing all available products
 function showTable() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        console.table(res);
-        
-        takeOrder(res);
-    });
+  connection.query('SELECT * FROM products', function(err, res) {
+    if (err) throw err;
+    console.table(res);
+
+    takeOrder(res);
+  });
 }
 
 // Prompt customer for id of item they wish to purchase
 function takeOrder(items) {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "choice",
-            message: "What would you would like to purchase? Please enter item id or Q to quit:",
-            validate: function(val) {
-                return !isNaN(val) || val.toLowerCase() === "q";
-            }
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'choice',
+        message:
+          'What would you would like to purchase? Please enter item id or Q to quit:',
+        validate: function(val) {
+          return !isNaN(val) || val.toLowerCase() === 'q';
         }
-    ]).then(function(val) {
-        exitApp(val.choice);
-        var choiceId = parseInt(val.choice);
-        var product = checkInventory(choiceId, items);
-        
-        if (product) {
-            orderQuant(product);
-        } else {
-            console.log("\nItem not found.");
-            showTable();
-        }
+      }
+    ])
+    .then(function(val) {
+      exitApp(val.choice);
+      var choiceId = parseInt(val.choice);
+      var product = checkInventory(choiceId, items);
+
+      if (product) {
+        orderQuant(product);
+      } else {
+        console.log('\nItem not found.');
+        showTable();
+      }
     });
 }
 
 function exitApp(choice) {
-    if (choice.toLowerCase() === "q") {
-        console.log("Exiting...");
-        process.exit(0);
-    }
+  if (choice.toLowerCase() === 'q') {
+    console.log('Exiting...');
+    process.exit(0);
+  }
 }
 
 function checkInventory(choiceId, items) {
-    for (let i = 0; i < items.length; i++) {
-        if (items[i].item_id === choiceId) {
-            return items[i];
-        }
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].item_id === choiceId) {
+      return items[i];
     }
-    return null;
+  }
+  return null;
 }
 
 function orderQuant(product) {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "quantity",
-            message: "How many would you like?  Please enter quantity:",
-            validate: function(val) {
-                return val > 0 || val.toLowerCase() === "q";
-            }
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'quantity',
+        message: 'How many would you like?  Please enter quantity:',
+        validate: function(val) {
+          return val > 0 || val.toLowerCase() === 'q';
         }
-    ]).then(function(val) {
-        exitApp(val.quantity);
-        var quantity = parseInt(val.quantity);
-        
-        if (quantity > product.stock_quantity) {
-            console.log("\nNot enough in stock.  Please try a smaller quantity.");
-            showTable();
-        } else {
-            purchaseItem(product, quantity);
-        }
+      }
+    ])
+    .then(function(val) {
+      exitApp(val.quantity);
+      var quantity = parseInt(val.quantity);
+
+      if (quantity > product.stock_quantity) {
+        console.log('\nNot enough in stock.  Please try a smaller quantity.');
+        showTable();
+      } else {
+        purchaseItem(product, quantity);
+      }
     });
 }
 
 function purchaseItem(product, quantity) {
-    connection.query(
-        "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
-        [quantity, product.item_id],
-        function(err, res) {
-            console.log("\nConfirmation:  You purchased " + quantity + " " + product. product_name);
-            showTable();
-        }
-    );
+  connection.query(
+    'UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?',
+    [quantity, product.item_id],
+    function(err, res) {
+      console.log(
+        '\nConfirmation:  You purchased ' +
+          quantity +
+          ' ' +
+          product.product_name
+      );
+      showTable();
+    }
+  );
 }
-
 
 // inquirer.prompt([
 //   {
 //     type: "input",
 //     name: "itemID",
-//     message: "What would you would like to purchase? Please enter item id:"  
+//     message: "What would you would like to purchase? Please enter item id:"
 //   },
 //   // Prompt for quantity to purchase
 //   {
@@ -120,10 +129,9 @@ function purchaseItem(product, quantity) {
 //     if (results[i].item_id === answer.choice) {
 //       chosenItem = results[i]
 //     }
-    
+
 //   }
 // })
-
 
 // //Prints products table
 // function fetchProducts(res){
@@ -140,24 +148,18 @@ function purchaseItem(product, quantity) {
 //   for(var row in result){
 //       res.write("<tr>");
 //       for(var column in result[row]){
-//           res.write("<td><label>" + result[row][column] + "</label></td>");       
+//           res.write("<td><label>" + result[row][column] + "</label></td>");
 //       }
-//       res.write("</tr>");         
+//       res.write("</tr>");
 //   }
 //   res.write("</table>");
 // });
 // }
-
-
-
-
-
-
 
 // Check stock of item to be purchased
 
 // If quant is insufficient, notify customer
 
 // If quant is sufficient, fill the order
-  // Update the SQL database
-  // Show customer the total cost of their purchase
+// Update the SQL database
+// Show customer the total cost of their purchase
